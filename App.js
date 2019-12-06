e1f97ce2bf88c6a0c8580f49badac708eb4b1e51
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-12-06 18:49:19 +0600

    Todo App Markup

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Main from './app/components/MainComponent'
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (
+      <Main />
+    );
+  }
+}
+ 
+export default App;
